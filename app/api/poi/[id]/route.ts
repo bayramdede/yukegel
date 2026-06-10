17f62460029e44bb3ad864a759972c501ed41988
Bayram Dede <bayramdede@gmail.com>
2026-06-10 21:31:02 +0300

    auto: 2026-06-10 21:31:02

--- app/api/poi/[id]/route.ts
+++ app/api/poi/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { getServiceSupabase } from '../../../../lib/auth';
+import { getServerSupabase, getServiceSupabase } from '../../../../lib/auth';
 
 export const runtime = 'nodejs';
 
