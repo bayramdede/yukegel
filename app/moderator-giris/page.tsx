a1f97005bc9daeba07011cc225a361e142f98e21
Bayram Dede <bayramdede@gmail.com>
2026-07-29 10:09:39 +0300

    auto: 2026-07-29 10:09:39

--- app/moderator-giris/page.tsx
+++ app/moderator-giris/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 import { useState } from 'react';
 import { createClient } from '../../lib/supabase';
-import { useRouter } from 'next/navigation';
 
 const supabase = createClient();
 
