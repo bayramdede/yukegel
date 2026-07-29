ef742c6edb9189b9fc18d62e909bac7d08676773
Bayram Dede <bayramdede@gmail.com>
2026-07-29 09:24:13 +0300

    auto: 2026-07-29 09:24:13

--- app/giris/page.tsx
+++ app/giris/page.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { createClient } from '../../lib/supabase';
 import { useRouter, useSearchParams } from 'next/navigation';
 import { Suspense } from 'react';
