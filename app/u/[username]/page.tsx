1c6ee887e762badccfe8ca5e2a769d2047961971
Bayram Dede <bayramdede@gmail.com>
2026-07-29 19:33:20 +0300

    auto: 2026-07-29 19:33:20

--- app/u/[username]/page.tsx
+++ app/u/[username]/page.tsx
@@ -2,6 +2,7 @@
 import { useState, useEffect } from 'react';
 import { createClient } from '../../../lib/supabase';
 import { useParams } from 'next/navigation';
+import { girisAdresi } from '../../../lib/redirect';
 
 const supabase = createClient();
 
